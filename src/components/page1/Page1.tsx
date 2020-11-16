755198651d1a2cf41d186bc81025f8fa6a81b409
kalinin <prozaik81-2@yandex.ru>
2020-11-16 21:06:02 +0300

    add form to page1

--- src/components/page1/Page1.tsx
+++ src/components/page1/Page1.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Page1() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [msg, setMsg] = useState('');
+
+    function submit() {
+        console.log(name, email, msg);
+    }
+
     return (
         <>
-            p1
+            <div className="form">
+                <input type="text" placeholder="name" onChange={ e => setName(e.target.value) }/>
+                <br/>
+                <input type="email" placeholder="email" onChange={ e => setEmail(e.target.value) }/>
+                <br/>
+                <textarea placeholder="message" onChange={ e => setMsg(e.target.value) }></textarea>
+                <br/>
+                <button onClick={ submit }>submit</button>
+            </div>
         </>
     );
 }
